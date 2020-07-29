825384c2907a833211d4c951368882404575b2e9
yoshihiro.yoshida <yoshihiro.yoshida@stanby.inc>
2020-07-29 22:54:49 +0900

    add: section2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # udemy
+
+https://www.udemy.com/course/python-beginner/learn/lecture/8304822#announcements
---
+++ section2.py
@@ -0,0 +1,11 @@
+# ctrl + R -> run
+# ctrl + D -> debug
+
+a = 'test'
+b = a
+c = b
+print(c)
+
+# 対話型shellの抜け方 exit() or ctrl + D
+# terminalは clear できれいになる
+
