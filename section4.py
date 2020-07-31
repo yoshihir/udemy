bb01ce9a40007ce3c6c70a8fbc64164e555df525
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-07-31 10:04:09 +0900

    Feat: end lesson 23

--- section4.py
+++ section4.py
@@ -54,7 +54,78 @@ a.extend(b)
 print(a)
 
 # 18 リストのメソッド
+r = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+print(r.index(3))
+print(r.count(3))
 
+if 5 in r:
+    print('exist')
 
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+
+s = 'My name is Mike'
+to_split = s.split(' ')
+print(to_split)
+
+x = ''.join(to_split)
+print(x)
+
+print(help(list))
+
+# 19 リストのコピー
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j =', j)
+print('i =', i)
+
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+y[0] = 100
+print('y =', y)
+print('x =', x)
+
+# 20 リストの使い所
+seat = []
+min = 0
+max = 5
+
+print(min <= len(seat) < max)
+seat.append('p')
+print(min <= len(seat) < max)
+seat.pop()
+print(min <= len(seat) < max)
+
+# 21 タプル型
+t = (1, 2, 3, 4, 5)
+print(help(tuple))
+## appendやpopがないlistのイメージ
+
+t = ([1, 2, 3], [4, 5, 6])
+print(t)
+
+# 22 タプルのアンパッキング
+num_tuple = (10, 20)
+x, y = num_tuple
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+a = 100
+b = 200
+print(a, b)
+a, b = b, a
+print(a, b)
+
+# 23 タプルの使い所
+## appendとかされたくないところで使う(valのイメージ)
+
+# 24 辞書型
 
