6a7743f327ea847c927b6cb265a28121fc56a111
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-08-04 22:27:31 +0900

    Feat: add 45 lesson

--- section5.py
+++ section5.py
@@ -68,6 +68,81 @@ if not is_ok:
     print(1)
 
 # 37. 値が入っていない判定をするテクニック
+# False, 0, [], (), {}, set()
+is_ok = ''
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
 
+# 38. Noneを判定する場合
+is_empty = None
+if is_empty is not None:
+    print('None!!')
+
+## isはオブジェクトの比較を行っている
+
+# 39. while文とcontinue文とbreak文
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+count = 0
+while True:
+    if count < 5:
+        break
+
+    if count == 2:
+        count += 1
+        continue
+
+    print(count)
+    count += 1
+
+# 40. while else文
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+else:
+    print('done')
+
+# 41. input関数
+# while True:
+#     word = input('Enter:')
+#     if word == 'ok':
+#         break
+#     print('next')
+
+# 42. for文とbreak文とcontinue文
+some_list = [1, 2, 3, 4, 5]
+
+for i in some_list:
+    print(i)
+
+for s in 'abcde':
+    print(s)
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        continue
+    print(word)
+
+# 43. for else文
+## elseはfor終了時に表示される
+for fruit in ['apple', 'banana', 'orange']:
+    print(fruit)
+else:
+    print('I')
+
+# 44. range関数
+for i in range(10):
+    print(i)
+
+for _ in range(10):
+    print('hello')
+
+# 45. enumerate関数
 
 
