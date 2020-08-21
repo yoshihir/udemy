290554663d1aea6cde419d33d952b7d10cfa1adc
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-08-21 21:04:36 +0900

    Feat: end lesson 90

--- 90.py
+++ 90.py
@@ -1,1 +1,23 @@
 # 90. 特殊メソッド
+class Word(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return 'Word!!!!!'
+
+    def __len__(self):
+        return len(self.text)
+
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
+
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
+
+
+
+w = Word('text')
+w2 = Word('###############')
+print(len(w))
+print(w + w2)
