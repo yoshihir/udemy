da18c2d0e46736d4e71263a6c3ca2b242be0c9b4
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-08-23 23:17:06 +0900

    Feat: end lesson 92

--- section8.py
+++ section8.py
@@ -1,8 +1,7 @@
 # 91. ファイルの作成
-f = open('test.txt', 'w')
-f.write('Test\n')
-print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
-f.close()
-
 # 92. withステートメントでファイルをopenする
+with open('test.txt', 'w') as f:
+    f.write('Test\n')
+print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
+
 
