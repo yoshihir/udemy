53020136c6fe8baae22258505f86cd5cee83903e
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-08-24 20:48:22 +0900

    Feat: end lesson 93

--- section8.py
+++ section8.py
@@ -4,4 +4,24 @@ with open('test.txt', 'w') as f:
     f.write('Test\n')
 print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
 
+# 93. ファイルの読み込み
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
+# with open('test.txt', 'w') as f:
+#     f.write(s)
+
+with open('test.txt', 'r') as f:
+    while True:
+        # 2個ずつ読み込む(パケット読み込みとか決まっているバイト数のときに使う)
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
+
 
