e19362984497b9462f632e78f7fb4ed7955a2a7e
yoshihiro.yoshida <marchwind0079@gmail.com>
2020-09-03 23:12:59 +0900

    Feat: end lesson 97

--- section8.py
+++ section8.py
@@ -27,7 +27,6 @@ with open('test.txt', 'r') as f:
 
         ## f.seek(5)で読み込む位置を進めさせられる。でも使わないかも
 
-
 # 95. 書き込み読み込みモード
 ## w+やr+で書き込みができ、さらに読み込みができるモードになる。書き込みの場合、seekで初期位置に移動してからreadしないと見れない。使うか?
 # with open('test.txt', 'w+') as f:
@@ -37,11 +36,23 @@ with open('test.txt', 'r') as f:
 
 # 96. テンプレート
 import string
+
 with open('design/email_template.txt') as f:
     t = string.Template(f.read())
 
 contents = t.substitute(name='Mike', contents='How are you?')
 print(contents)
 
+# 97. CSVファイルへの書き込みと読み込み
+import csv
 
+with open('test.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writerow({'Name': 'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
 
+with open('test.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
